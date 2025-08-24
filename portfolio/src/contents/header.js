a5c7a4cbf15252141bd51ce5415f7013038610a5
phuwanat kunwaraapiphat <84963824+phuwanat48@users.noreply.github.com>
2025-08-24 13:46:05 +0700

    Delete portfolio directory

--- portfolio/src/contents/header.js
+++
@@ -1,9 +0,0 @@
-import FileName from'../assets/Resume.pdf'
-
-export const data = {
-    name:"Phuwanat Kunwaraapiphat",
-    title:"DEV/Front-end",
-    caption:"Hello there! 👋 I'm Phuwanat, currently studying Computer Science at Kasetsart University, Kamphaeng Saen Campus, and I'm absolutely passionate about coding.",
-    btnText:"Veiw Resume",
-    link:FileName,
-}
